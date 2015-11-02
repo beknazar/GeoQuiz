1c7360f7e2139a8ccec659a55393680f1e2261da
beknazar <beknazar23@gmail.com>
2015-11-02 21:26:40 +0900

    Initial commit all files

---
+++ app/src/main/java/kr/co/unist/geoquiz/TrueFalse.java
@@ -0,0 +1,35 @@
+package kr.co.unist.geoquiz;
+
+/**
+ * Created by abdik on 10/31/15.
+ */
+
+public class TrueFalse {
+    private int mQuestion;
+    // this variable will hold a resource ID for a string
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+
+}
+
